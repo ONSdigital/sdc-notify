39c6101910686dc484ba7b42825bfa48717f2c49
Nick Gravgaard <me@nickgravgaard.com>
2016-11-25 15:04:27 +0000

    Define PORT at the module level

--- app.py
+++ app.py
@@ -10,6 +10,7 @@ from sqlalchemy import Column, DateTime, Integer, String, Text, desc
 # service name (initially used for sqlite file name and schema name)
 SERVICE_NAME = 'sdc-notify'
 ENVIRONMENT_NAME = os.getenv('ENVIRONMENT_NAME', 'dev')
+PORT = int(os.environ.get('PORT', 5001))
 
 app = Flask(__name__)
 
@@ -155,5 +156,4 @@ if __name__ == '__main__':
         recreate_database()
 
     # Start server
-    port = int(os.environ.get('PORT', 5001))
-    app.run(debug=True, host='0.0.0.0', port=port)
+    app.run(debug=True, host='0.0.0.0', port=PORT)
